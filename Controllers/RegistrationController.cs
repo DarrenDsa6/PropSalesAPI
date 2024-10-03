0ad33a05e8358d63968f7a6b07fe04cc583145fa
dards <dards@AUTO8XMAVIAPQQ1>
2024-10-03 17:19:13 +0530

    Add project files.

---
+++ Controllers/RegistrationController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PropertySales.Data;
+using PropertySales.Models.Domain;
+using PropertySalesAPI.Models.ViewModels;
+
+namespace PropSalesAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegistrationController : ControllerBase
+    {
+        private readonly PropertySalesDbContext _context;
+
+        public RegistrationController(PropertySalesDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPut("Broker")]
+        public IActionResult RegisterBroker([FromBody] RegisterBrokerRequest request)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var broker = new Broker
+            {
+                Name = request.Name,
+                UserName = request.UserName,
+                Password = request.Password, // Consider hashing passwords
+                ContactNumber = request.ContactNumber,
+                Address = request.Address,
+                Pincode = request.Pincode,
+                AdhaarCard = request.AdhaarCard
+            };
+
+            _context.Brokers.Add(broker);
+            _context.SaveChanges();
+            return Ok(broker);
+        }
+
+        [HttpPost("User")]
+        public IActionResult RegisterUser([FromBody] RegisterBrokerRequest request)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var user = new User
+            {
+                Name = request.Name,
+                UserName = request.UserName,
+                Password = request.Password, // Consider hashing passwords
+                ContactNumber = request.ContactNumber,
+                Address = request.Address,
+                Pincode = request.Pincode,
+                AdhaarCard = request.AdhaarCard
+            };
+
+            _context.Users.Add(user);
+            _context.SaveChanges();
+            return Ok(user);
+        }
+    }
+}
---
+++ Models/ViewModels/RegisterBrokerRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace PropertySalesAPI.Models.ViewModels
+{
+    public class RegisterBrokerRequest
+    {
+        public string Name { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public long ContactNumber { get; set; }
+        public string Address { get; set; }
+        public long Pincode { get; set; }
+        public long AdhaarCard { get; set; }
+    }
+}
